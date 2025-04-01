81b799f74577fc7a4c3aa2943b88659942a62e08
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-01 13:47:15 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,21 @@
-public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+public class PracticeProblem{
+	static int sumOfDigits(int x){
+		if(x < 0) x *= -1;
+		if(x < 10){
+			return x;
+		}
+		return sumOfDigits(x / 10) + x % 10;
 	}
 
-	
+	static int factorial(int n){
+		if(n == 0){
+			return 1;
+		}else if(n < 0){
+			return 0;
+		}
+		
+		return n * factorial(n-1);
+	}
 
+	public static void main(String args[]){}
 }
